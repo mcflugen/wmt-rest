7ac99b3e1100df4c797fa6156d96a4ca0d318a45
mcflugen <huttone@colorado.edu>
2016-05-27 13:29:21 -0600

    Updated to use openid.

--- scripts/wsgi.py
+++ scripts/wsgi.py
@@ -1,7 +1,19 @@
+#! /usr/bin/env python
+from os import path
+from flask.ext.openid import OpenID
+from openid.extensions import pape
+
 from wmt.flask import create_app
 
+import local_settings
 
-if __name__ == "__main__":
-    from os import path
 
-    create_app(wmt_root_path=path.abspath(path.dirname(__file__))).run()
+application = create_app(settings_override=local_settings,
+                         wmt_root_path=path.abspath(path.dirname(__file__)))
+oid = OpenID(application, safe_roots=[], extension_responses=[pape.Response])
+
+
+#if __name__ == "__main__":
+#    from os import path
+#
+#    create_app(wmt_root_path=path.abspath(path.dirname(__file__))).run()
